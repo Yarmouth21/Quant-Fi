0127c7e460b24abad3d910c6d289f6e137f118bf
Yarmouth21 <jurgenstyll@icloud.com>
2025-11-24 16:19:01 +0100

    Début du fix des graphes

--- Monte/Monte.py
+++ Monte/Monte.py
@@ -13,7 +13,7 @@ def monte(tracker='AAPL', simulations=200, days=252):
     for i in range(simulations):
         st = spot
         for j in range(days):
-            st = st * np.exp((returns.mean() - 0.5 * variance) + std_dev * np.sqrt(1/days)*np.random.normal(0,1))
+            st = st * np.exp((returns.mean() - 0.5 * variance) + std_dev *np.random.normal(0,1))
             resultats[i, j] = st
     return resultats
 
@@ -30,8 +30,14 @@ plt.plot(highest, color='blue')
 plt.plot(lowest,color='red')
 #plt.fill_between(highest, lowest, color='gray', alpha=0.2)
 plt.title("Simulation moyenne et la plus optimiste")
-plt.subplot(2,1,1)
+plt.subplot(4,2,1)
 for courbe in res:
     plt.plot(courbe)
 plt.title('Simulations de Monte Carlo')
+#plt.subplot(2,1,2)
+seuil_haut = int(0.2*len(res))
+#val = np.sort(res)[seuil_haut]
+#val_sel = res[res >= seuil_haut]
+#for ele in val_sel:
+#    plt.plot(ele)
 plt.show()
